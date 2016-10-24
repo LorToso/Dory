8e6ecdb08b617ccb6b47fc33517b694597a89932
Lorenzo Toso <LorToso@gmail.com>
2016-10-24 14:16:17 +0200

    Added all different Fragments and layouts (as stubs atm, but hey, they work)

--- app/src/main/java/com/doryapp/dory/extendedViews/ScreenSlidePagerAdapter.java
+++ app/src/main/java/com/doryapp/dory/extendedViews/ScreenSlidePagerAdapter.java
@@ -3,16 +3,23 @@ package com.doryapp.dory.extendedViews;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v13.app.FragmentStatePagerAdapter;
 
+import com.doryapp.dory.fragments.CodeFragment;
+import com.doryapp.dory.fragments.FriendsFragment;
 import com.doryapp.dory.fragments.MapFragment;
-import com.doryapp.dory.fragments.ScreenSlidePageFragment;
+import com.doryapp.dory.fragments.ProfileFragment;
+import com.doryapp.dory.fragments.SettingsFragment;
 
 public class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
 
 
-    private Fragment[] pages = new Fragment[]{new ScreenSlidePageFragment(), new MapFragment(), new ScreenSlidePageFragment(), new ScreenSlidePageFragment(), new ScreenSlidePageFragment()};
+    private Fragment[] pages = new Fragment[]{
+            new ProfileFragment(),
+            new MapFragment(),
+            new FriendsFragment(),
+            new CodeFragment(),
+            new SettingsFragment()};
 
     public ScreenSlidePagerAdapter(FragmentManager fm) {
         super(fm);
---
+++ app/src/main/java/com/doryapp/dory/fragments/CodeFragment.java
@@ -0,0 +1,25 @@
+package com.doryapp.dory.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.doryapp.dory.R;
+
+/**
+ * Created by Lorenzo Toso on 24.10.2016.
+ */
+
+public class CodeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_code, container, false);
+
+        return rootView;
+    }
+}
+
