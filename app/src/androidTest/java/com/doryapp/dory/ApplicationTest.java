2d3461498bc975369c0d3c5bd8a41a284d249ce9
Lorenzo Toso <LorToso@gmail.com>
2016-10-28 12:53:36 +0200

    Added a test that checks whether the server can be reached

--- app/src/androidTest/java/com/doryapp/dory/ApplicationTest.java
+++ app/src/androidTest/java/com/doryapp/dory/ApplicationTest.java
@@ -4,8 +4,10 @@ import android.content.res.Resources;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.doryapp.dory.apiCalls.ApiCall;
 import com.doryapp.dory.apiCalls.AuthedApiCall;
 import com.doryapp.dory.apiCalls.DoesUserExistCall;
+import com.doryapp.dory.apiCalls.NopCall;
 import com.google.firebase.auth.FirebaseAuth;
 
 import junit.framework.Assert;
@@ -29,6 +31,18 @@ public class ApplicationTest {//extends ActivityTestRule<MainActivity> {
         firebaseAuth = FirebaseAuth.getInstance();
 //        String url = resources.getString(R.string.server_url);
 //        int i = 0;
+    }
+
+
+    @Test
+    public void canConnectToServer()
+    {
+        new NopCall(con).onException(new ApiCall.OnException() {
+            @Override
+            public void handle(Exception ex) {
+                Assert.fail();
+            }
+        }).executeSynchronously();
     }
 
     @Test
