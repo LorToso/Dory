73a9a036ad3bb9e00dd6d3f3e659345ed165e4af
Lorenzo Toso <LorToso@gmail.com>
2016-12-21 23:47:24 +0100

    Added a logo to the QR-Code

--- app/src/main/java/com/doryapp/dory/fragments/CodeFragment.java
+++ app/src/main/java/com/doryapp/dory/fragments/CodeFragment.java
@@ -2,6 +2,7 @@ package com.doryapp.dory.fragments;
 
 import android.app.Fragment;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
@@ -14,7 +15,6 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
-
 
 public class CodeFragment extends Fragment {
 
@@ -30,39 +30,65 @@ public class CodeFragment extends Fragment {
         ImageView imageView = (ImageView) rootView.findViewById(R.id.codeView);
         if(imageView != null)
         {
-            try {
-                Bitmap bitmap = encodeAsBitmap("test");
-                imageView.setImageBitmap(bitmap);
-            } catch (WriterException e) {
-                e.printStackTrace();
-            }
+            Bitmap bitmap = getFriendshipCode();
+            imageView.setImageBitmap(bitmap);
         }
 
         return rootView;
     }
 
-    Bitmap encodeAsBitmap(String str) throws WriterException {
-        BitMatrix result;
-        int width = getMaximumWidth();
+    private Bitmap getFriendshipCode() {
+        Bitmap logo = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.logo);
+
         try {
-            result = new MultiFormatWriter().encode(str,
-                    BarcodeFormat.QR_CODE, width, width, null);
-        } catch (IllegalArgumentException iae) {
-            // Unsupported format
-            return null;
+            Bitmap bitmap = generateQRFromString("testtesttesttesttest");
+            overlayLogo(bitmap, logo);
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
         }
-        int w = result.getWidth();
-        int h = result.getHeight();
-        int[] pixels = new int[w * h];
-        for (int y = 0; y < h; y++) {
-            int offset = y * w;
-            for (int x = 0; x < w; x++) {
-                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+        return null;
+    }
+
+    private void overlayLogo(Bitmap bitmap, Bitmap logo) {
+
+        int logoWidth = bitmap.getWidth()/3;
+        int logoHeight = bitmap.getHeight() / 3;
+
+        Bitmap scaledLogo = Bitmap.createScaledBitmap(logo, logoWidth, logoHeight, false);
+
+        int[] pixels = new int[scaledLogo.getWidth() * scaledLogo.getHeight()];
+        scaledLogo.getPixels(pixels, 0, logoWidth, 0, 0, logoWidth, logoHeight);
+        bitmap.setPixels(pixels, 0, logoWidth, logoWidth, logoHeight, logoWidth, logoHeight);
+    }
+
+    Bitmap generateQRFromString(String str) throws WriterException {
+        try {
+            BitMatrix result;
+            int width = getMaximumWidth();
+            int height = width;
+            try {
+                result = new MultiFormatWriter().encode(str,
+                        BarcodeFormat.QR_CODE, width, height, null);
+            } catch (IllegalArgumentException iae) {
+                // Unsupported format
+                return null;
             }
+            int w = result.getWidth();
+            int h = result.getHeight();
+            int[] pixels = new int[w * h];
+            for (int y = 0; y < h; y++) {
+                int offset = y * w;
+                for (int x = 0; x < w; x++) {
+                    pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+                }
+            }
+            Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, w, h);
+            return bitmap;
+        } catch (WriterException ignored) {
         }
-        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        bitmap.setPixels(pixels, 0, width, 0, 0, w, h);
-        return bitmap;
+        return null;
     }
 
     private int getMaximumWidth() {
