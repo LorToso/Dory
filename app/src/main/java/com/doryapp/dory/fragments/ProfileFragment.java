4d026545cc66e52a873dd3cf6136a4a6445df75a
Lorenzo Toso <LorToso@gmail.com>
2016-12-23 19:22:25 +0100

    Minor refactoring

--- app/src/main/java/com/doryapp/dory/fragments/ProfileFragment.java
+++ app/src/main/java/com/doryapp/dory/fragments/ProfileFragment.java
@@ -10,7 +10,6 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.doryapp.backend.myApi.model.DoryUser;
